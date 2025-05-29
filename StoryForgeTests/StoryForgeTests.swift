327f29585847a59ac60910b3eb73e61dfc6d991b
stevenrichter16 <stevenrichter16@gmail.com>
2025-05-28 20:59:04 -0500

    Initial Commit

---
+++ StoryForgeTests/StoryForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryForgeTests.swift
+//  StoryForgeTests
+//
+//  Created by Steven Richter on 5/28/25.
+//
+
+import Testing
+@testable import StoryForge
+
+struct StoryForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
